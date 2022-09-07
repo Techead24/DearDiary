ef668173ab595e936e15552a79a20ecb3f34c639
Techead24 <74743818+Techead24@users.noreply.github.com>
2022-09-07 22:51:52 +0100

    Created login and logout page

--- journal/settings.py
+++ journal/settings.py
@@ -126,6 +126,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
-
+LOGIN_REDIRECT_URL = 'entry-list'
